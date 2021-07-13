bbd4bb0c7d71866d02a3f5b56f37e47dbd0e89b8
= <=>
2021-07-13 20:36:18 +0300

    add validation and progress

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
-$(window).scroll(function(){
 
-    if($(this).scrollTop()>=200){
-        $("#navbar").addClass("noTransparrent");
-     }
-    else{
-        $("#navbar").removeClass("noTransparrent");
-     }
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 
-});
+  window.onscroll = function() {scrollFunction()};
 
+  function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById("navbar").classList.add("noTransparrent")
+    } else {
+      document.getElementById("navbar").classList.remove("noTransparrent")
+    }
+  }
 
-$(document).ready(function(){
-    $('.circle').circleProgress({
-        startAngle: -Math.PI / 2,
-        fill: "#0575e6"
-    }).on('circle-animation-progress', function(event, progress, stepValue) {
-        $(this).find('span').html(Math.round(stepValue * 100) + '%');
-    });
-});
