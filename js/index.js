5cacceaeab34b774bb893428f6d1cecd7f6e8e67
= <=>
2021-07-14 14:40:55 +0300

    add jquery

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
     'use strict'
@@ -29,4 +28,33 @@
       document.getElementById("navbar").classList.remove("noTransparrent")
     }
   }
+
+  $(window).scroll(function(){
+
+    if($(this).scrollTop()>=200){
+        $("#navBar").addClass("noTransparrent");
+     }
+    else{
+        $("#navBar").removeClass("noTransparrent");
+     }
+
+});
+
+$(document).ready(function(){
+    $("a.scroll").on('click', function(event) {
+
+        var hash = this.hash;
+
+        $('html, body').animate({ scrollTop: $(hash).offset().top - 90 }, 800, function(){});
+    
+    });
+
+    $('.circle').circleProgress({
+        startAngle: -Math.PI / 2,
+        fill: "#0575e6"
+    }).on('circle-animation-progress', function(event, progress, stepValue) {
+        $(this).find('span').html(Math.round(stepValue * 100) + '%');
+    });
+
+});
 
